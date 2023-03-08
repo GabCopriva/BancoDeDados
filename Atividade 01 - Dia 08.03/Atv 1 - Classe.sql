0d9ef10c73be99cef7f072d55970332ebba5d923
GabCopriva <contatogabrielcopriva@gmail.com>
2023-03-08 16:53:20 -0300

    Atualizado

--- Atividade 01 - Dia 08.03/Atv 1 - Classe.sql
+++ Atividade 01 - Dia 08.03/Atv 1 - Classe.sql
@@ -1,1 +1,22 @@
 CREATE DATABASE db_generation_game_online;
+
+USE db_generation_game_online;
+
+-- Tabela
+CREATE TABLE tb_classes (
+	id_classes int not null auto_increment,
+    nome_classes varchar (100) not null,
+    descricao_classes varchar (200) not null,
+    idade_classes varchar (200) not null,
+    primary key (id_classes)
+);
+
+-- Insert
+INSERT INTO tb_classes (nome_classes, descricao_classes, idade_classes) values
+('Guerreiro', 'Jovem cavaleiro', 'Vinte'),
+('Feiticeira', 'Jovem feiticeira', 'Vinte'),
+('Curandeiro', 'Jovem curandeiro', 'Vinte'),
+('Ninja', 'Jovem ninja', 'Vinte'),
+('Mago', 'Jovem mago', 'Vinte');
+
+SELECT * FROM tb_classes;
--- Atividade 01 - Dia 08.03/Atv 1 - Perso.sql
+++ Atividade 01 - Dia 08.03/Atv 1 - Perso.sql
@@ -1,1 +1,31 @@
-CREATE TABLE tb_personagens
+CREATE TABLE tb_personagens (
+id_personagens int auto_increment,
+nome_personagens varchar (255) not null,
+tipo varchar (255) not null,
+poderataque int,
+poderdefesa int,
+id_classe int not null,
+primary key (id_personagens),
+foreign key (id_classe) references tb_classes (id_classe)
+);
+
+-- Insert
+INSERT INTO tb_personagens (nome_personagens, tipo, poderataque, poderdefesa, id_classe) values
+('Aslan', 'Perto', 4000, 1200, 1),
+('Lila', 'Médio', 1500, 2000, 2),
+('Sam', 'Perto', 1000, 2500, 3),
+('Tami', 'Perto', 2500, 1900, 4),
+('Roan', 'Médio', 1500, 2000, 5),
+('Lael', 'Perto', 3000, 1000, 6),
+('Joan', 'Perto', 4000, 1200, 7),
+('Anir', 'Perto', 4000, 1200, 8);
+
+SELECT * FROM tb_personagens WHERE poderataque > 2000;
+SELECT * FROM tb_personagens WHERE poderdefesa BETWEEN 1000 AND 2000;
+
+SELECT * FROM tb_personagens WHERE nome_personagens LIKE '%C%';
+
+SELECT * FROM tb_personagens INNER JOIN tb_classes ON tb_personagens.id_classes;
+SELECT * FROM tb_personagens INNER JOIN tb_classes ON tb_personagens.id_classes = tb_classes.id_classe WHERE tb_classes.nome_classe = "Guerreiro"
+
+
